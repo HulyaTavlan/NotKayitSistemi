dfce064e9e79e50613b13e587faf27724ed62d68
HulyaTavlan <karatashly@gmail.com>
2023-08-06 20:12:53 +0300

    NOT KAYIT SISTEMI
    C#,ADO.NET

---
+++ Not_Kayit_Sistemi/FrmOgrenciDetay.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace Not_Kayit_Sistemi
+{
+    public partial class FrmOgrenciDetay : Form
+    {
+        public FrmOgrenciDetay()
+        {
+            InitializeComponent();
+        }
+
+        public string numara;
+
+        SqlConnection baglanti = new SqlConnection("Data Source=DESKTOP-ORAR8O2;Initial Catalog=DbNotKayit;Integrated Security=True");
+        private void FrmOgrenciDetay_Load(object sender, EventArgs e)
+        {
+            LblNumara.Text = numara;
+
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("Select * From TBLDERS where OGRNUMARA=@p1", baglanti);
+            komut.Parameters.AddWithValue("@p1", numara);
+            SqlDataReader dr = komut.ExecuteReader();
+            while (dr.Read())
+            {
+                LblAdSoyad.Text = dr[2].ToString() + " " + dr[3].ToString();
+                LblSinav1.Text = dr[4].ToString();
+                LblSinav2.Text = dr[5].ToString();
+                LblSinav3.Text = dr[6].ToString();
+                LblOrtalama.Text = dr[7].ToString();    
+                LblDurum.Text = dr[8].ToString();
+
+            }
+            baglanti.Close();
+        }
+    }
+}
---
+++ Not_Kayit_Sistemi/FrmOgretmenDetay.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Not_Kayit_Sistemi
+{
+    public partial class FrmOgretmenDetay : Form
+    {
+        public FrmOgretmenDetay()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection baglanti = new SqlConnection("Data Source=DESKTOP-ORAR8O2;Initial Catalog=DbNotKayit;Integrated Security=True");
+
+        private void FrmOgretmenDetay_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'dbNotKayitDataSet.TBLDERS' table. You can move, or remove it, as needed.
+            this.tBLDERSTableAdapter.Fill(this.dbNotKayitDataSet.TBLDERS);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("INSERT INTO TBLDERS (OGRNUMARA,OGRAD,OGRSOYAD) values (@P4,@P5,@P6)", baglanti);
+            komut.Parameters.AddWithValue("@P4", MskNumara.Text);
+            komut.Parameters.AddWithValue("@P5", TxtAd.Text);
+            komut.Parameters.AddWithValue("@P6", TxtSoyad.Text);
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            MessageBox.Show("Öğrenci Sisteme Eklendi");
+            this.tBLDERSTableAdapter.Fill(this.dbNotKayitDataSet.TBLDERS);
+
+        }
+    }
+}
